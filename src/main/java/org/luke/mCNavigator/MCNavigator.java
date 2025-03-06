a1f81fee289815162f4fabdfd4bbbf4d5a5f64b7
luke0630 <ryuto-0915@outlook.jp>
2025-03-06 17:45:02 +0900

    init

---
+++ src/main/java/org/luke/mCNavigator/MCNavigator.java
@@ -0,0 +1,25 @@
+package org.luke.mCNavigator;
+
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MCNavigator extends JavaPlugin {
+    @Getter
+    private static MCNavigator instance;
+
+    @Override
+    public void onLoad() {
+        instance = this;
+    }
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
